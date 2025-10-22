eff306cb6ce2372f937a41181657f1b1792867bc
Windows <windows@example.com>
2025-10-22 12:06:46 +0700

    update say hello with parameter name

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/maskmxx81-code/golang-training
+module github.com/maskmxx81-code/golang-training/v2
 
 go 1.25.1
--- say-hello.go
+++ say-hello.go
@@ -1,5 +1,5 @@
 package golangtraining
 
-func SayHello() string {
-	return "Hello, World!"
+func SayHello(name string) string {
+	return "Hello, " + name
 }
